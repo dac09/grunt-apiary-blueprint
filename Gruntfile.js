7f5abc60ed40ddd66a196282a95f126cdd823e05
dmtrs <dmtrs@localhost.localdomain>
2015-03-30 23:38:20 +0300

    Added apiary publish task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,42 @@
-// Project configuration.
+apiary = require('./config.js');
 
 module.exports = function(grunt){
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
-grunt.initConfig({
-  concat: {
-    options: {
-      separator: '\n \n',
-    },
-    dist: {
-      src: ["source/*.apib", "source/*.md"],
-      dest: 'dist/built.apib'
-    }
-  },
-  watch: {
-  scripts: {
-    files: ['source/*.apib'],
-    tasks: ['concat'],
-    options: {
-      spawn: false,
-    },
-  },
-},
-  clean: ['dist/*.apib'],
-});
-  grunt.registerTask('default', [ 'clean','watch' ]);
-
+    grunt.initConfig({
+        env: {
+            dev: {
+                APIARY_API_KEY: apiary.config.key
+            }
+        },
+        exec: {
+            publish: [ 'apiary', 'publish',
+                [ '--api-name', apiary.config.name ].join('='),
+                [ '--path', 'dist/apiary.apib' ].join('=')
+            ].join(' ')
+        },
+        concat: {
+            options: {
+                separator: '\n \n',
+            },
+            dist: {
+                src: ["source/*.apib", "source/*.md"],
+                dest: 'dist/apiary.apib'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['source/*.apib'],
+                tasks: ['concat','exec:publish'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+        clean: ['dist/*.apib'],
+    });
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('default', [ 'env:dev','clean','watch' ]);
 }
--- README.md
+++ README.md
@@ -2,22 +2,20 @@ grunt-apiary-blueprint
 ======================
 
 A grunt setup for dealing with apiary blueprints. Makes the workflow much easier, by concatonating multiple blueprints in the
-source folder to a single file - dist/built.apib
+source folder to a single file - dist/built.apib and also publishing.
 
-## How to use this project
 
-### Clone the repo, then install dependencies
+1. Install the [Apiary Client](http://client.apiary.io/)
+2. Clone the repo, then install dependencies
 
 ``` 
 cd grunt-apiary-blueprint/
 npm install
 ```
 
-### Start grunt in bash
-``` grunt ```
+3. Copy `config.example.js` to `config.js` and set the `name` to match `apiary publish --api-name=name`
+and `key` the apiary api key you would normally export to a enviroment variable `APIARY_API_KEY`
 
-### Save your blueprints inside /source
-
-### Deploy 'compiled' blueprint - dist/built.apib
-- Use apiary gem https://github.com/apiaryio/apiary-client 
-- Or run any of the local processors 
+4. Start grunt in bash ``` grunt ```
+5. Save your blueprints inside /source as `\*.apib` files
+6. Repeat **Step 5**
